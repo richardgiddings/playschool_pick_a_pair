aa96412f0e1f3a90767a5f5e83dfcd444fc84333
Richard Giddings <rmpgiddings@gmail.com>
2026-03-13 09:45:27 +0000

    First attempt at the game

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,128 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Tile } from "../tile/tile";
+
+import { useState } from 'react';
+
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "Playschool pick a pair" },
+        { name: "description", content: "Playschool pick a pair game" },
+    ];
 }
 
-export default function Home() {
-  return <Welcome />;
+
+export async function clientLoader() {
+
+    // width * height must be even
+    const height = 4;
+    const width = 4;
+    const numbers = (height * width) / 2 
+
+    // create array with numbers twice
+    var numsOne = Array.from({length: numbers}, (e, i)=> i);
+    var numsTwo = Array.from({length: numbers}, (e, i)=> i);
+    var nums = numsOne.concat(numsTwo);
+
+    // randomise the nums array
+    var ranNums = [];
+    var i = nums.length;
+    var j = 0;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+
+    // intialise other variables
+    let tilesClicked = Array(); // stores the actual tiles that were clicked
+    let matches = Array(); // stores keys of tiles that have been matched
+    let currentlyClicked = Array(height*width).fill(0); // stores position of clicked tiles
+    let index = 0; // for assigning keys
+
+    return {ranNums, height, width, tilesClicked, matches, currentlyClicked, index};
+}
+
+
+export default function Home({loaderData}: Route.ComponentProps) {
+
+    let {ranNums, height, width, tilesClicked, matches, currentlyClicked, index} = loaderData;
+    const [canClickMore, setCanClickMore] = useState(true);
+    const [message, setMessage] = useState("");
+
+    // get a row at a time of length width
+    const arrayChunk = (arr: any, n: number) => {
+        const array = arr.slice();
+        const chunks = [];
+        while (array.length) chunks.push(array.splice(0, n));
+        return chunks;
+    };
+
+    const tileClicked = (keyval: any) => {
+
+        if(matches.includes(keyval.key)) {
+            return
+        }
+        
+        if (currentlyClicked[keyval.key] == 1) {
+            currentlyClicked[keyval.key] = 0;
+            if(tilesClicked[0].key === keyval.key) {
+                tilesClicked.splice(0, 1);
+            }
+            else {
+                tilesClicked.splice(1, 1);
+            }
+        }
+        else if (currentlyClicked[keyval.key] == 0 && tilesClicked.length < 2) {
+            currentlyClicked[keyval.key] = 1
+            tilesClicked.push(keyval);
+        }
+
+        // now check if we have a match
+        if(tilesClicked.length === 2) {
+            if(tilesClicked[0].value === tilesClicked[1].value) {
+                matches.push(tilesClicked[0].key);
+                matches.push(tilesClicked[1].key);
+
+                // reset
+                tilesClicked.length = 0;
+                currentlyClicked.fill(0);
+                setCanClickMore(true);
+            }
+            else {
+                setCanClickMore(false);
+            }
+        }
+        else {
+            setCanClickMore(true);
+        }
+
+        // Have we won?
+        if (matches.length === (width * height)) {
+            setMessage("Congratulations!!!!");
+        }
+  }
+
+
+  return (
+        <div>
+            <div className="block">
+                <table>
+                    <tbody>
+                        { arrayChunk([...ranNums], width).map((row, i) => (
+                        <tr key={i}>
+                            {row.map((col: number, i: number) => (
+                            <td><Tile key={index++} tileProps={{ tileIndex: index, value: col, matches: matches, canClickMore: canClickMore }} tileClicked={tileClicked}/></td>
+                            ))}
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="block">
+                <button onClick={() => window.location.reload()}>Refresh</button>
+            </div>
+            <div className="block">{message}</div>
+        </div>
+    );
 }
---
+++ app/tile/tile.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export function Tile(props: any) {
+
+    const value = props.tileProps.value;
+    const tileIndex = props.tileProps.tileIndex;
+    const matches = props.tileProps.matches;
+    const canClickMore = props.tileProps.canClickMore;
+
+    const [show, setShow] = useState(false);
+
+    const clicked = (key: any, value: any) => {
+        if((!show && canClickMore) || show) {
+            props.tileClicked({key: key, value: value});
+            setShow(!show);
+        }
+    }
+
+    return (
+        <div className={show ? 'tile tile-show': 'tile tile-hidden'} 
+            onClick={() => {
+                if(!matches.includes(tileIndex)) {
+                    clicked(tileIndex, value) 
+                }
+            }}>
+            {show ? value: ""}
+        </div>
+    );
+}
